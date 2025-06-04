2d1c0f559186d7a37af255fbb728dd4af0bbb82b
decade-afk <3995409050@qq.com>
2025-06-04 11:29:21 +0800

    fix bug

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@
 
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
