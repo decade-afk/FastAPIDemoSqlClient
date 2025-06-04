e75bf8cc752b461fb7f051d899a289e21aeda251
decade-afk <3995409050@qq.com>
2025-06-04 17:32:20 +0800

    fix

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# 添加项目根目录到 Python 路径
+root_path = Path(__file__).parent.parent.resolve()
+sys.path.insert(0, str(root_path))
 import os
 import mysql.connector
 import pytest
